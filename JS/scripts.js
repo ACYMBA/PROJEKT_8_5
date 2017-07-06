a6ddda38b678b7ba10c4a69d1127454743298080
albert <albert.cymbaluk@gmail.com>
2017-07-06 14:25:53 +0200

    wysyłka

---
+++ JS/scripts.js
@@ -0,0 +1,10 @@
+// nowe komantarze są w modzie
+ /* var name = prompt('Enter your name');
+  alert('Hello, ' + name);
+ console.log('Hello, ' + name);
+*/
+var a = prompt ('Podaj wartość a');
+  var  h = prompt ('Podaj wartość h');
+     var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert('Hello, ' + triangleArea);
